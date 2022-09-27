15419bec0f1c887342cf5edee6d9ddadedb443c7
Nasseef Abukamail <abukamai@ohio.edu>
2022-09-26 21:15:56 -0400

    minor edit

--- set.cc
+++ set.cc
@@ -19,19 +19,23 @@ int main(int argc, char const *argv[]) {
     int data[] = {1, 8, 3, 2};
     set<int> mySet;
     mySet.insert(data, data + 4);
-    mySet.insert(9);
-    mySet.insert(9); //should not be added
-    mySet.insert(10);
-    mySet.insert(5);
-    mySet.insert(6);
-
-    mySet.erase(9);
-
+   
+    
     for (auto it = mySet.begin(); it != mySet.end(); it++) {
         cout << *it << " ";
     }
     cout << endl;
-    
+    int data2 [] = {1,3,9};
+    set<int> otherSet;
+    otherSet.insert(data2, data2+8);
+
+    for (auto it = otherSet.begin(); it != otherSet.end(); it++) {
+        cout << *it << " ";
+    }
+    cout << endl;
+    cout << (mySet <= otherSet) << endl;
+
+    return 0;
     //find
     auto it = mySet.find(8);
     if (it == mySet.end()) {
@@ -54,6 +58,7 @@ int main(int argc, char const *argv[]) {
     }
     cout << endl;
     
+
     unordered_multiset<int> mySet3;
     mySet3.insert(data, data + 4);
     mySet3.insert(9);
