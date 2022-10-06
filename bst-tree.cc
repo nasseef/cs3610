d40ebab35e0bcee2e8854b63af2b9f783d9e12c0
Nasseef Abukamail <abukamai@ohio.edu>
2022-10-06 10:28:41 -0400

    Added removeNode and removeLargest functions

--- bst-tree.cc
+++ bst-tree.cc
@@ -29,6 +29,7 @@ class BST {
     void insert(int newData) { treeInsert(root, newData); };
     void clear() { treeClear(root); }
     BST copy() { return BST(treeCopy(root)); }
+    void remove(int target){removeNode(root, target);}
 
    private:
     BSTNode *root;
@@ -37,10 +38,13 @@ class BST {
     void printDepth(BSTNode *root, int depth);
     void treeClear(BSTNode *&root);
     BSTNode *treeCopy(BSTNode *root);
+    void removeLargest(BSTNode *&root, int &largest);
+    void removeNode(BSTNode *&root, int target);
 };
 
 int main(int argc, char const *argv[]) {
     BST tree;
+    
     tree.insert(30);
     tree.insert(10);
     tree.insert(40);
@@ -50,9 +54,18 @@ int main(int argc, char const *argv[]) {
     tree.insert(50);
     tree.insert(15);
     tree.insert(37);
-    
+
     tree.print();
     cout << endl;
+    int data;
+    for (size_t i = 0; i < 4; i++)
+    {
+        cout << "Enter a number to remove: ";
+        cin >> data;
+        tree.remove(data);
+        tree.print();
+    }
+    
     BST tree2 = tree.copy();
     tree.clear();
     tree2.print();
@@ -78,7 +91,7 @@ void BST::preorderPrint(BSTNode *root) {
 //     // else the element is in the tree
 // }
 
-//Non-recursive version
+// Non-recursive version
 void BST::treeInsert(BSTNode *&root, int newData) {
     bool done = false;
     BSTNode *cursor = root;
@@ -91,14 +104,14 @@ void BST::treeInsert(BSTNode *&root, int newData) {
             if (cursor->left == nullptr) {
                 cursor->left = new BSTNode(newData);
                 done = true;
-            } else{
+            } else {
                 cursor = cursor->left;
             }
         } else if (newData > cursor->data) {
             if (cursor->right == nullptr) {
                 cursor->right = new BSTNode(newData);
                 done = true;
-            } else{
+            } else {
                 cursor = cursor->right;
             }
         } else
@@ -134,4 +147,41 @@ BSTNode *BST::treeCopy(BSTNode *root) {
     leftPtr = treeCopy(root->left);
     rightPtr = treeCopy(root->right);
     return new BSTNode(root->data, leftPtr, rightPtr);
+}
+
+void BST::removeLargest(BSTNode *&root, int &largest) {
+    BSTNode *removePtr;
+    if (root == nullptr) {
+        return;
+    }
+    if (root->right == nullptr) {
+        largest = root->data;
+        removePtr = root;
+        root = root->left;
+        delete removePtr;
+    } else
+        removeLargest(root->right, largest);
+}
+
+void BST::removeNode(BSTNode *&root, int target) {
+    BSTNode *removePtr;
+    if (root == nullptr) {return;}
+    if (target < root->data){
+        removeNode(root->left, target);
+    }
+    else if (target > root->data){
+        removeNode(root->right, target);
+    }
+    else if (root->left == nullptr) {  // the target node is found
+        removePtr = root;              // move the root to the right
+        root = root->right;
+        delete removePtr;
+    } else if (root->right == nullptr) {  // move the root to the left
+        removePtr = root;
+        root = root->left;
+        delete removePtr;
+    } else{ // The left and right have nodes find and delete the largest on the
+            // left and replace the root value with the one being deleted.
+        removeLargest(root->left, root->data);
+    }
 }
